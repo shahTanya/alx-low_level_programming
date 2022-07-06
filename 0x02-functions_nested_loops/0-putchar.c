fd36638fd6c9d2cecb7f93572210e1b03451b69f
shahTanya <shahtania19@gmail.com>
2022-07-06 16:20:27 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ */
+int main(void)
+
+{
+	char arr[8] = "_putchar";
+	int i;
+	for (i = 0; i < 8; i++)
+
+	{
+		_putchar(arr[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
+
