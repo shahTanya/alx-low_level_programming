da5cae027d7d67c6439c08bd918644079cafddb6
shahTanya <shahtania19@gmail.com>
2022-07-07 20:20:34 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,17 +6,15 @@
  *    * Return: Always 0.
  */
 int main(void)
-
 {
 	char arr[8] = "_putchar";
 	int i;
 	for (i = 0; i < 8; i++)
 
 	{
-		_putchar(arr[i]);
+	_putchar(arr[i]);
 	}
-
 	_putchar('\n');
+
 	return (0);
 }
-
