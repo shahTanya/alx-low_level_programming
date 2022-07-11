2087c171d6f6e712b8210c18d022d7c2ec039500
shahTanya <shahtania19@gmail.com>
2022-07-11 16:50:13 -0700

    git commit -m

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-/*
- * reset_to_98 -> a function to update the reference of a pointer
- *@n: pointer to n to be updated
+/**
+ * reset_to_98 - main
+ * @n: - pinter to int
  */
-void reset_to_98(int *n);
 
+void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
