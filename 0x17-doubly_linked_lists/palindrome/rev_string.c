44d37f3abb290538ad0f469fcbb6cb5bf1e17adf
shahTanya <shahtania19@gmail.com>
2023-01-10 19:32:04 -0800

    rev_string.c

---
+++ 0x17-doubly_linked_lists/palindrome/rev_string.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * rev_string - reverses the order of a string
+ * @s: string pointer
+ */
+void rev_string(char *s)
+{
+	int counter = 0, temp, len, low_index = 0, shift = 0;
+
+	/*move from first character of array s to null...*/
+	/*...to get the array length*/
+	for (; *(s + shift);)
+	{
+		shift++;
+	}
+	len = shift;
+
+	/*move from last character of array s to first, replacing...*/
+	/*...the characters with first to last characters respectively*/
+	for (shift--; counter < (len / 2); shift--)
+	{
+		temp = *(s + shift);
+		*(s + shift) = s[low_index];
+		s[low_index] = temp;
+		low_index++;
+		counter++;
+	}
+}
