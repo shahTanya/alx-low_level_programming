9c131616233dba9e922801d640377cd8f16f6e5b
shahTanya <shahtania19@gmail.com>
2023-02-11 16:49:41 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#!/#!/bin/bash
+#!/bin/bash
 wget -P .. https://raw.githubusercontent.com/shahTanya/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
